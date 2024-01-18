3d3bc4a10a38a5175ea273cc7b1c71d8ab5b1db3
Unity01 <76080041+raiym2048@users.noreply.github.com>
2024-01-18 13:13:06 +0600

    test

--- src/main/java/com/example/library_online/controller/TypeController.java
+++ src/main/java/com/example/library_online/controller/TypeController.java
@@ -10,6 +10,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/type")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class TypeController {
     private final TypeService typeService;
 
